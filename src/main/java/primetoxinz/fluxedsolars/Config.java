16587c7107a918fa6a8944de78a2211da01f9d25
primetoxinz <primetoxinzz@gmail.com>
2016-01-11 18:04:54 -0500

    added energy configs

--- src/main/java/primetoxinz/fluxedsolars/Config.java
+++ src/main/java/primetoxinz/fluxedsolars/Config.java
@@ -8,19 +8,32 @@ import net.minecraft.block.Block;
 import net.minecraft.init.Blocks;
 import net.minecraftforge.common.config.Configuration;
 import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
+import primetoxinz.fluxedsolars.tile.SolarPanelType;
 
 public class Config extends Configuration {
+	public static final int[] default_energypertick= new int[]{64,512,4096,32786};
+	public static final int[] default_energystorage= new int[]{64000,512000,4096000,32786000};
 	public Config(File suggestedConfigurationFile) {
 		super(suggestedConfigurationFile);
 	}
 
 
-
+	
 	public void preInit(FMLPreInitializationEvent e) {
 		this.load();
-		String category = "";
+		String category = "Energy";
 		String name = "";
 		String comment = "";
+		for(SolarPanelType type:SolarPanelType.VALUES)  {
+			name = type.getName()+"_energypertick";
+			comment = "Change energy production of " + type.getName() + " tier";
+			type.energypertick=getInt(name, category, default_energypertick[type.ordinal()], 0, Integer.MAX_VALUE, comment);
+			name = type.getName()+"_energyenergystorage";
+			comment = "Change energy storage of " + type.getName() + " tier";
+			type.energystorage=getInt(name, category, default_energystorage[type.ordinal()], 0, Integer.MAX_VALUE, comment);
+
+		}
 		this.save();
 	}
 }
+
--- src/main/java/primetoxinz/fluxedsolars/tile/SolarPanelType.java
+++ src/main/java/primetoxinz/fluxedsolars/tile/SolarPanelType.java
@@ -1,24 +1,26 @@
 package primetoxinz.fluxedsolars.tile;
 
 import net.minecraft.util.IStringSerializable;
+import net.minecraftforge.common.config.Configuration;
+import primetoxinz.fluxedsolars.Config;
+import primetoxinz.fluxedsolars.FluxedSolars;
 
 public enum SolarPanelType implements IStringSerializable {
-	REDSTONE(0,"redstone", 64,64000),
-	GOLD(1,"gold",512,512000),
-	BLAZE(2,"blaze",4096,4096000),
-	ENDER(3,"ender",32768, 32768000);
-
-	SolarPanelType(int id,String name, int energypertick, int energystorage) {
+	REDSTONE(0,"redstone"),
+	GOLD(1,"gold"),
+	BLAZE(2,"blaze"),
+	ENDER(3,"ender");
+	
+	SolarPanelType(int id,String name) {
 		this.name = name;
 		this.id = id;
-		this.energypertick = energypertick;
-		this.energystorage = energystorage;
 	}
 	private final String name;
 	private final int id;
 	public int energypertick;
 	public int energystorage;
-	
+	public static Config config = FluxedSolars.config;
+
 	@Override	
 	public String getName() {
 		return name;
